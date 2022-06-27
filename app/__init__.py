e5112be36104be6730786d0b6b3fa29e5d6c84c2
yakuzaramen <yakuzaramen@yandex.ru>
2022-06-27 18:16:41 +0300

    Editor profile + alfa img

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_migrate import Migrate
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 
@@ -15,4 +18,4 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from . import routes
+from app import routes, models
--- app/models.py
+++ app/models.py
@@ -3,8 +3,6 @@ from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from app import db, login
-
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
