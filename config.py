f9b9d6d720e949af0219bc3a9f36448a715d4ca6
yakuzaramen <yakuzaramen@yandex.ru>
2022-07-03 10:40:17 +0300

    Followers

--- config.py
+++ config.py
@@ -2,10 +2,11 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.bd')
+                              'sqlite:///' + os.path.join(basedir, 'app.bd')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
